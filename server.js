f7064b9e943987b105e2eccb92cee810708d23d2
Alex Delgado <delgadoalex@icloud.com>
2018-07-10 17:06:09 -0500

    Commit 3 - update pokemon get route set

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 // require express
 const express = require('express'); 
-const app = express(); 
+const app = express();
+const Pokemon = require('./models/pokemon'); 
 
 // PORT ACCESS
 const port = 5000;
+
+
+app.get('/pokemon', (req, res) => {
+  res.render('Welcome to the Pokemon App!');
+}); 
 
 app.listen(port, () => {
   console.log('pokemon express is running on port: ', port);
