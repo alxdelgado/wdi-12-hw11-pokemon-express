f5a3883642fa17f728b1d6042dd60ca36768a3ca
Alex Delgado <delgadoalex@icloud.com>
2018-07-10 19:04:33 -0500

    Commit 9 - created show  views of each pokemon

--- server.js
+++ server.js
@@ -20,7 +20,9 @@ app.get('/pokemon', (req, res) => {
 
 // Show route for pokemon 
 app.get('/pokemon/:id', (req, res) => {
-  res.send(req.params.id);
+  res.render('show.ejs', {
+    pokemon: Pokemon[req.params.id] 
+  });
 });
 
 app.listen(port, () => {
